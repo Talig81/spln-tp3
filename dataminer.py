b3571706b4628931bf3201b53d4d2d5f6fcfeaa8
Manuel Moreno <rotundix81@gmail.com>
2020-06-19 15:48:06 +0100

    new datasets

--- dataminer.py
+++ dataminer.py
@@ -1,22 +1,52 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib
+import urllib.request
+import re
 
 
 
-#file = open('../data/divisaosilabica.csv','w')
-#file.write("palavra,tipo,divisao_silabica\n")
+abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+"s","t","u","v","w","x","y","z"][1:]
 
 
-    
-    
-def main():
-    
-    r = requests.get('http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter=a')
+
+
+file = open('datasets/divisaosilabica.csv','wb')
+file.write(b"palavra,tipo,divisao_silabica\n")
+
+def getNumberPages(letter):
+    r = requests.get('http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter='+letter)
     soup = BeautifulSoup(r.content,"html.parser")
-    rows = soup.find(id="rollovertable").findAll("tr")
-    stffs = soup.find().findAll("p")
-    for s in stffs:
-        print("um")
+    paragraphs = soup.findAll("p")
+    matched = re.search(r'1 - 100 de (\d+) resultados',str(paragraphs[1].get_text),re.M|re.I)
+    quantity = int(matched.group(1))
+    return quantity
 
-main()
+def main():
+    for letra in abc:
+        pages = getNumberPages(letra)
+        conta = 0
+        for page_number in range(0, pages, 100):
+            print(f"{letra} - {page_number}")
+            response = urllib.request.urlopen(f'http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter={letra}&start={conta}')
+            data = response.read().decode('utf-8', 'ignore')
+            soup = BeautifulSoup(data,features="html.parser")
+            rows = soup.find(id="rollovertable").findAll("tr")
+            for row in rows:
+                cells = row.findAll("td")
+                if cells:
+                    # __import__('pdb').set_trace()
+                    palavra = cells[0].find('b').find('a').getText().encode()
+                    file.write(palavra)
+                    file.write(b',')
+                    tipo = cells[0].getText().split("(")[1].split(")")[0].encode()
+                    file.write(tipo)
+                    file.write(b',')
+                    divisaosilabica = cells[1].getText().encode()
+                    file.write(divisaosilabica.replace(b"&middot;",b"."))
+                    file.write(b"\n")
+            break
+
+
+if __name__ == "__main__":   
+    main()
---
+++ dataminer2.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import re
+from multiprocessing import Pool
+
+
+
+ABC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+"s","t","u","v","w","x","y","z"]
+
+
+def getNumberPages(letter):
+    r = requests.get('http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter='+letter)
+    soup = BeautifulSoup(r.content,"html.parser")
+    paragraphs = soup.findAll("p")
+    matched = re.search(r'1 - 100 de (\d+) resultados',str(paragraphs[1].get_text),re.M|re.I)
+    quantity = int(matched.group(1))
+    return quantity
+
+
+def escreve_letra(letra):
+        pages = getNumberPages(letra)
+        
+        with open(f'datasets/divisaosilabica_{letra}.csv', 'wb') as file:
+            for page_number in range(0, pages, 100):
+                print(f"{letra} - {page_number}")
+                response = urllib.request.urlopen(f'http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter={letra}&start={page_number}')
+                soup = BeautifulSoup(response.read().decode('utf-8', 'ignore'), features="html.parser")
+                rows = soup.find(id="rollovertable").findAll("tr")
+            
+                for row in rows:
+                    cells = row.findAll("td")
+                    if not cells:
+                        return
+
+                    file.write(b"palavra,tipo,divisao_silabica\n")
+                    palavra = cells[0].find('b').find('a').getText().encode()
+                    file.write(palavra)
+                    file.write(b',')
+                    tipo = cells[0].getText().split("(")[1].split(")")[0].encode()
+                    file.write(tipo)
+                    file.write(b',')
+                    divisaosilabica = cells[1].getText().encode()
+                    
+                    file.write(divisaosilabica.replace(b"&middot;",b"."))
+
+                    file.write(b"\n")
+
+
+def main():
+    with Pool(26) as p:
+        print(p.map(escreve_letra, ABC))
+
+if __name__ == "__main__":   
+    main()
