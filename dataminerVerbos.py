9af51402356dc6d84db42898928d7e937612e2cb
Manuel Moreno <rotundix81@gmail.com>
2020-06-25 22:30:53 +0100

    verbos conjugados

--- dataminerVerbos.py
+++ dataminerVerbos.py
@@ -11,25 +11,31 @@ def listVerbs():
     verbList = set()
     for letter in abc:
         with open('datasets/divisaosilabica_'+letter+'.csv', 'rb') as f:
-            for line in f:
-                line = line.decode()
-                matched = re.search(r'(\w+), \(verbo\)(.*)',line, re.I)
-                if matched is None:
-                    continue
-                verbList.add(matched.group(1))
-                
+            with open('datasets/verbos_'+letter+'.txt', 'wb') as f2:
+                for line in f:
+                    line = line.decode()
+                    matched = re.search(r'(\w+), \(verbo\)(.*)',line, re.I)
+                    if matched is None:
+                        continue
+                    getVerbConjugado(matched.group(1),f2)
+
     return list(sorted(verbList))
 
-def addVerbos(verbList):
-    for verb in verbList:
-        print(verb)
-        
-        response = urllib.request.urlopen(f'https://conjuga-me.net/verbo-' + quote(verb))
+def getVerbConjugado(verb,f2):
+    verbs = []
+    response = urllib.request.urlopen(f'https://conjuga-me.net/verbo-' + quote(verb))
+    soup = BeautifulSoup(response.read().decode('utf-8', 'ignore'), features="html.parser")
+    if soup.find('div',{'class':'conjugation-header'}):
+        for tag in soup.findAll('div',{'class':'conjugation'}):
+                f2.write(tag.getText().encode())
+                f2.write(b", (verbo), -\n")
+                
+
         
 
 def main():
      verbList = listVerbs()
-     addVerbos(verbList)
+
 
 if __name__ == "__main__":   
     main()
